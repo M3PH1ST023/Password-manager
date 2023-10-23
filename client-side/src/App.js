6e842fda0fb0e363a64096e6c2c255c5500ae185
M3PH1ST023 <107043613+M3PH1ST023@users.noreply.github.com>
2023-10-23 20:28:50 +0530

    put endpoint and add.js works

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
+import View from "./components/View";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Sidebar />
+            <Router>
+                <Routes>
+                    <Route path="/home" Component={Home} />
+                    <Route path="/add" Component={Add} />
+                    <Route path="/edit" Component={Edit} />
+                    <Route path="/show" Component={View} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- server-side-py/server.py
+++ server-side-py/server.py
@@ -1,25 +1,29 @@
 from flask import Flask
-
+from flask_cors import CORS
 import sys
 sys.path.append('api')
 from Creds import *
 from Users import *
 
 app = Flask(__name__)
+CORS(app, origins="http://127.0.0.1:3000")
+app.config['CORS_HEADER']='Content-Type'
 
 #### Routes ####
 
 app.route('/api/v1/addCreds', methods=['POST'])(addCreds)
 app.route('/api/v1/getCreds', methods=['GET'])(getCreds)
 app.route('/api/v1/getCredsById', methods=['GET'])(getCredById)
+app.route('/api/v1/updateCred', methods=['PUT'])(putCreds)
 app.route('/api/v1/deleteCredById', methods=['DELETE'])(deleteCredById)
 
 app.route('/api/v1/addUser', methods=['POST'])(addUser)
 app.route('/api/v1/getUser', methods=['GET'])(getUser)
 app.route('/api/v1/getUserById', methods=['GET'])(getUserById)
+app.route('/api/v1/updateUser', methods=['PUT'])(putUser)
 app.route('/api/v1/deleteUserById', methods=['DELETE'])(deleteUserById)
 
 #### Routes ####
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
